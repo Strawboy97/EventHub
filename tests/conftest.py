4df89f66080749c69161121151e0d904cdc8a0cd
Colin Masterson <colinmasterson97@hotmail.com>
2026-04-12 13:45:40 +0100

    Updated fixture to use with statements and handle if json file does not exist

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,24 @@
 import json
+import os.path
 
 import pytest
 
 
 @pytest.fixture
 def user_data(faker):
-    data = {'email': faker.email(), 'password': faker.password()}
+    data = {"email": faker.email(), "password": faker.password()}
 
-    existing_file = json.load(open('userData.json', 'r'))
+    file_path = "userData.json"
+
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            existing_file = json.load(f)
+    else:
+        existing_file = []
 
     existing_file.append(data)
 
-    json.dump(existing_file, open('userData.json', 'w'))
+    with open(file_path, "w") as f:
+        json.dump(existing_file, f, indent=2)
 
     return data
