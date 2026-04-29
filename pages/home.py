dda1338311b59d8767d58dd5e1db50e681eab121
Colin Masterson <colinmasterson97@hotmail.com>
2026-04-29 19:45:15 +0100

    added UI test for home page

--- pages/home.py
+++ pages/home.py
@@ -1,17 +1,23 @@
 import allure
-
+from config import BASE_URL
 
 class HomePage:
     def __init__(self, page):
         self.page = page
+        self.logo = page.get_by_role("link", name="EventHub")
+        self.nav_bar = page.locator('nav')
+        self.nav_items = page.locator('nav .hidden .rounded-lg')
+        self.home_button = page.get_by_test_id("nav-home")
         self.home_heading = page.get_by_role(
             "heading", name="Discover & Book Amazing Events"
         )
+        self.events_button = page.get_by_test_id("nav-events")
+        self.bookings_button = page.get_by_test_id("nav-bookings")
         self.logout_button = page.get_by_role("button", name="Logout")
 
     @allure.step('Wait for Home Page to Load')
-    def navigation_check(self):
-        self.page.wait_for_url("https://eventhub.rahulshettyacademy.com/")
+    def wait_for_home_page(self):
+        self.page.wait_for_url(f"{BASE_URL}")
 
     @allure.step('Click the logout Button')
     def logout(self):
---
+++ tests/test_home.py
@@ -0,0 +1,63 @@
+import allure
+from playwright.sync_api import Page, expect
+
+from pages.login import LoginPage
+
+
+@allure.title('Test Logo is Displayed')
+@allure.epic("EventHub")
+@allure.feature("UI")
+@allure.story("Home Page")
+@allure.severity(allure.severity_level.MINOR)
+@allure.testcase('TC-HOME-001')
+@allure.label('owner', 'Colin M')
+def test_logo_displays(login_user):
+    home_page = login_user
+    home_page.wait_for_home_page()
+
+    expect(home_page.logo).to_be_visible()
+
+
+@allure.title('Test Nav Bar Items are Present')
+@allure.epic("EventHub")
+@allure.feature("UI")
+@allure.story("Home Page")
+@allure.severity(allure.severity_level.MINOR)
+@allure.testcase('TC-HOME-002')
+@allure.label('owner', 'Colin M')
+def test_all_nav_items_present(login_user):
+    home_page = login_user
+    home_page.wait_for_home_page()
+
+
+    expect(home_page.nav_items).to_have_count(6)
+
+
+@allure.title('Test Home button is Highlighted')
+@allure.epic("EventHub")
+@allure.feature("UI")
+@allure.story("Home Page")
+@allure.severity(allure.severity_level.MINOR)
+@allure.testcase('TC-HOME-003')
+@allure.label('owner', 'Colin M')
+def test_home_button_highlight(login_user):
+    home_page = login_user
+    home_page.wait_for_home_page()
+
+    expect(home_page.home_button).to_contain_class('bg-indigo-50')
+
+
+@allure.title('Test Logout is Successful')
+@allure.epic("EventHub")
+@allure.feature("UI")
+@allure.story("Home Page")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.testcase('TC-HOME-004')
+@allure.label('owner', 'Colin M')
+def test_successful_logout(page:Page,login_user):
+    home_page = login_user
+    home_page.wait_for_home_page()
+    home_page.logout()
+    login_page = LoginPage(page)
+
+    expect(login_page.login_heading).to_be_visible()
