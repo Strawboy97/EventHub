7fd8d289752c00877c94612582f1521cf946e744
Colin Masterson <colinmasterson97@hotmail.com>
2026-04-30 21:19:16 +0100

    Added Events Page Tests

--- pages/events.py
+++ pages/events.py
@@ -1,3 +1,10 @@
 class EventPage:
     def __init__(self, page):
         self.page = page
+        self.events_heading = page.get_by_role("heading", name="Upcoming Events")
+        self.empty_search_heading = page.get_by_role("heading", name="No events found")
+        self.search_box = page.get_by_role("textbox", name="Search events, venues…")
+        self.event_card = page.get_by_test_id("event-card")
+
+    def search_for_event(self, event_name:str):
+        self.search_box.fill(event_name)
